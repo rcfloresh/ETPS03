5dfc12e0d31de0f85d51b4431740e95151ed1083
Roberto Flores <2530892009@mail.utec.edu.sv>
2023-10-11 20:32:49 -0600

    Update

--- nprojectetps03/lib/screens/home_screen.dart
+++ nprojectetps03/lib/screens/home_screen.dart
@@ -1,19 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import '../screens/signin_screen.dart';
 import 'package:flutter/material.dart';
+import '../utils/color_utils.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              hexStringToColor("CB2B93"),
+              hexStringToColor("9546C4"),
+              hexStringToColor("5E61F4")
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+
+
+
+          
+
+        /*
+        Logout button
+
         child: ElevatedButton(
           child: Text("Logout"),
           onPressed: () {
@@ -23,8 +44,14 @@ class _HomeScreenState extends State<HomeScreen> {
                   MaterialPageRoute(builder: (context) => SignInScreen()));
             });
           },
-        ),
+        ),*/
+
+
+        
       ),
     );
   }
+
+
 }
+
