2673c000e813d843f77e8d05d696373dbf8aeb35
Enmanuel Enrique Bonilla Melara <67129217+Ebonilla1808@users.noreply.github.com>
2023-10-17 23:36:19 -0600

    nueva funcionalidad a pantalla de busqueda

--- nprojectetps03/lib/screens/home_screen.dart
+++ nprojectetps03/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../screens/pharmacy_screen.dart';
+import '../utils/color_utils.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,76 +14,127 @@ class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _medicineController = TextEditingController();
   String? _selectedDepartment;
   String? _selectedMunicipality;
+  List<String> _medicineList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _medicineController,
-              decoration: InputDecoration(
-                labelText: 'Medicamento',
-              ),
-            ),
-            StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance.collection('deptMuni').snapshots(),
-              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) return const Text('Cargando...');
-                final departments = snapshot.data!.docs.map((doc) => doc.id).toList();
-                return DropdownButton<String>(
-                  hint: Text('Selecciona un departamento'),
-                  value: _selectedDepartment,
-                  items: departments.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _selectedDepartment = newValue;
-                      _selectedMunicipality = null;
-                    });
-                  },
-                );
-              },
-            ),
-            StreamBuilder<DocumentSnapshot>(
-              stream: FirebaseFirestore.instance.collection('deptMuni').doc(_selectedDepartment).snapshots(),
-              builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-                if (!snapshot.hasData || !snapshot.data!.exists) return const Text('Cargando...');
-                final municipalities = (snapshot.data!.get('municipalities') as List).cast<String>();
-                return DropdownButton<String>(
-                  hint: Text('Selecciona un municipio'),
-                  value: _selectedMunicipality,
-                  items: municipalities.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _selectedMunicipality = newValue;
-                    });
-                  },
-                );
-              },
-            ),
-            ElevatedButton(
-              child: Text("Buscar"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PharmacyScreen(medicineName: _medicineController.text, department: _selectedDepartment!, municipality: _selectedMunicipality!)),
-                );
-              },
-            ),
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "Inicio",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
+      ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                hexStringToColor("CB2B93"),
+                hexStringToColor("9546C4"),
+                hexStringToColor("5E61F4")
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance.collection('Medicamentos').snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (!snapshot.hasData) return const Text('Cargando...');
+                        _medicineList = snapshot.data!.docs.map((doc) => doc.id).toList();
+                        return Autocomplete<String>(
+                          optionsBuilder: (TextEditingValue textEditingValue) {
+                            if (textEditingValue.text == '') {
+                              return const Iterable<String>.empty();
+                            }
+                            return _medicineList.where((String option) {
+                              return option.toLowerCase().contains(textEditingValue.text.toLowerCase());
+                            });
+                          },
+                          onSelected: (String selection) {
+                            _medicineController.text = selection;
+                          },
+                        );
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance.collection('deptMuni').snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (!snapshot.hasData) return const Text('Cargando...');
+                        final departments = snapshot.data!.docs.map((doc) => doc.id).toList();
+                        return DropdownButton<String>(
+                          hint: Text('Selecciona un departamento'),
+                          value: _selectedDepartment,
+                          items: departments.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _selectedDepartment = newValue;
+                              _selectedMunicipality = null;
+                            });
+                          },
+                        );
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    StreamBuilder<DocumentSnapshot>(
+                      stream: FirebaseFirestore.instance.collection('deptMuni').doc(_selectedDepartment).snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                        if (!snapshot.hasData || !snapshot.data!.exists) return const Text('Cargando...');
+                        final municipalities = (snapshot.data!.get('municipalities') as List).cast<String>();
+                        return DropdownButton<String>(
+                          hint: Text('Selecciona un municipio'),
+                          value: _selectedMunicipality,
+                          items: municipalities.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _selectedMunicipality = newValue;
+                            });
+                          },
+                        );
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      child: Text("Buscar"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder:
+                              (context) => PharmacyScreen(medicineName:
+                          _medicineController.text, department:
+                          _selectedDepartment!, municipality:
+                          _selectedMunicipality!)),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              )
+          )
       ),
     );
   }
