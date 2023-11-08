575bb488211985cd8ec490c6413f0f79cc3742e2
Enmanuel Enrique Bonilla Melara <67129217+Ebonilla1808@users.noreply.github.com>
2023-11-07 20:41:29 -0600

    Mejoras

--- nprojectetps03/lib/screens/home_screen.dart
+++ nprojectetps03/lib/screens/home_screen.dart
@@ -19,170 +19,203 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: const Text(
-          "Inicio",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+        title: Text('Busqueda de medicamentos',
+            style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white)), // Cambiado el color del texto a blanco
       ),
       body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-                hexStringToColor("CB2B93"),
-                hexStringToColor("9546C4"),
-                hexStringToColor("5E61F4")
-              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("CB2B93"),
+          hexStringToColor("9546C4"),
+          hexStringToColor("5E61F4")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SafeArea(
+          // Agrega SafeArea aquí
           child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
-                child: Column(
-                  children: <Widget>[
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    StreamBuilder<QuerySnapshot>(
-                      stream: FirebaseFirestore.instance.collection('Medicamentos').snapshots(),
-                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                        if (!snapshot.hasData) return const Text('Cargando...');
-                        _medicineList = snapshot.data!.docs.map((doc) => doc.id).toList();
-                        return Autocomplete<String>(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+              // Cambiado el padding superior para evitar la superposición con la AppBar
+              child: Column(
+                children: <Widget>[
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection('Medicamentos')
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData) return const Text('Cargando...');
+                      _medicineList =
+                          snapshot.data!.docs.map((doc) => doc.id).toList();
+                      return Autocomplete<String>(
+                        optionsBuilder: (TextEditingValue textEditingValue) {
+                          if (textEditingValue.text == '') {
+                            return const Iterable<String>.empty();
+                          }
+                          return _medicineList.where((String option) {
+                            return option
+                                .toLowerCase()
+                                .contains(textEditingValue.text.toLowerCase());
+                          });
+                        },
+                        onSelected: (String selection) {
+                          _medicineController.text = selection;
+                        },
 
-                          optionsBuilder: (TextEditingValue textEditingValue) {
-                            if (textEditingValue.text == '') {
-                              return const Iterable<String>.empty();
-                            }
-                            return _medicineList.where((String option) {
-                              return option.toLowerCase().contains(textEditingValue.text.toLowerCase());
-                            });
-                          },
-                          onSelected: (String selection) {
-                            _medicineController.text = selection;
-                          },
-
-                          // Start Style
-                          fieldViewBuilder: (context, Controller, focusNode, onFieldSubmitted){
-                            return TextFormField(
-                              controller: Controller,
-                              focusNode: focusNode,
-                           //   onFieldSubmitted: onFieldSubmitted,
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.black), // Borde enfocado negro
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                hintText: 'Ingrese un medicamento'
+                        // Start Style
+                        fieldViewBuilder:
+                            (context, Controller, focusNode, onFieldSubmitted) {
+                          return TextFormField(
+                            controller: Controller,
+                            focusNode: focusNode,
+                            //   onFieldSubmitted: onFieldSubmitted,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.local_pharmacy,
+                                color: Colors.white70,
                               ),
-                            );
-                          },
-
-
-                          // End Style
-
-
-
-                        );
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    StreamBuilder<QuerySnapshot>(
-                      stream: FirebaseFirestore.instance.collection('deptMuni').snapshots(),
-                      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                        if (!snapshot.hasData) return const Text('Cargando...');
-                        final departments = snapshot.data!.docs.map((doc) => doc.id).toList();
-                        return DropdownButton<String>(
-                          hint: Text('Selecciona un departamento'),
-                          value: _selectedDepartment,
-                          items: departments.map((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              _selectedDepartment = newValue;
-                              _selectedMunicipality = null;
-                            });
-                          },
-                          // Start Style
-
-                        style: TextStyle(color: Colors.black), // Color del texto negro
-                        icon: Icon(Icons.arrow_drop_down), // Icono de flecha
-                        underline: Container(  // Borde inferior
-                        height: 2,
-                        color: Colors.black, // Color del borde inferior
-                        ),
-
+                              labelText:
+                                  "Ingrese el medicamento que desea consultar",
+                              labelStyle: TextStyle(
+                                  color: Colors.white.withOpacity(0.9)),
+                              filled: true,
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.never,
+                              fillColor: Colors.white.withOpacity(0.3),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30.0),
+                                  borderSide: const BorderSide(
+                                      width: 0, style: BorderStyle.none)),
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          );
+                        },
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection('deptMuni')
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData) return const Text('Cargando...');
+                      final departments =
+                      snapshot.data!.docs.map((doc) => doc.id).toList();
+                      return DropdownButtonFormField<String>(
                         decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        )
+                          prefixIcon: Icon(
+                            Icons.location_city,
+                            color: Colors.white70,
+                          ),
+                          labelText: "Selecciona un departamento",
+                          labelStyle: TextStyle(
+                              color: Colors.white.withOpacity(0.9)),
+                          filled: true,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          fillColor: Colors.white.withOpacity(0.3),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: const BorderSide(width: 0, style: BorderStyle.none)),
                         ),
+                        value: _selectedDepartment,
+                        items: departments.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedDepartment = newValue;
+                            _selectedMunicipality = null;
+                          });
+                        },
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  StreamBuilder<DocumentSnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection('deptMuni')
+                        .doc(_selectedDepartment)
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<DocumentSnapshot> snapshot) {
+                      if (!snapshot.hasData || !snapshot.data!.exists)
+                        return const Text('Cargando...');
+                      final municipalities =
+                      (snapshot.data!.get('municipalities') as List)
+                          .cast<String>();
+                      return DropdownButtonFormField<String>(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.location_city,
+                            color: Colors.white70,
+                          ),
+                          labelText: "Selecciona un municipio",
+                          labelStyle: TextStyle(
+                              color: Colors.white.withOpacity(0.9)),
+                          filled: true,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          fillColor: Colors.white.withOpacity(0.3),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: const BorderSide(width: 0, style: BorderStyle.none)),
+                        ),
+                        value: _selectedMunicipality,
+                        items: municipalities.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedMunicipality = newValue;
+                          });
+                        },
+                      );
+                    },
+                  ),
 
-                          // End Style
-                        );
-                      },
-                    ),
-
-
-
-
-
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    StreamBuilder<DocumentSnapshot>(
-                      stream: FirebaseFirestore.instance.collection('deptMuni').doc(_selectedDepartment).snapshots(),
-                      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-                        if (!snapshot.hasData || !snapshot.data!.exists) return const Text('Cargando...');
-                        final municipalities = (snapshot.data!.get('municipalities') as List).cast<String>();
-                        return DropdownButton<String>(
-                          hint: Text('Selecciona un municipio'),
-                          value: _selectedMunicipality,
-                          items: municipalities.map((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              _selectedMunicipality = newValue;
-                            });
-                          },
-                        );
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      child: Text("Buscar"),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder:
-                              (context) => PharmacyScreen(medicineName:
-                          _medicineController.text, department:
-                          _selectedDepartment!, municipality:
-                          _selectedMunicipality!)),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              )
-          )
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    child: Text("Buscar"),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PharmacyScreen(
+                                medicineName: _medicineController.text,
+                                department: _selectedDepartment!,
+                                municipality: _selectedMunicipality!)),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- nprojectetps03/lib/screens/pharmacy_screen.dart
+++ nprojectetps03/lib/screens/pharmacy_screen.dart
@@ -2,7 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../screens/signin_screen.dart';
 import '../screens/map_page.dart';
+import '../screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import '../utils/color_utils.dart';
 
 class PharmacyScreen extends StatelessWidget {
   final String medicineName;
@@ -14,15 +16,17 @@ class PharmacyScreen extends StatelessWidget {
   Future<DocumentSnapshot> getPharmacyData(String pharmacyName) async {
     return await FirebaseFirestore.instance.collection('Farmacias').doc(pharmacyName).get();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text('Farmacias para $medicineName'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text('Farmacias para $medicineName', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white)),  // Cambiado el color del texto a blanco
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.logout),
+            icon: Icon(Icons.logout, color: Colors.white),  // Cambiado el color del ícono a blanco
             onPressed: () {
               FirebaseAuth.instance.signOut().then((value) {
                 print("Signed Out");
@@ -33,40 +37,83 @@ class PharmacyScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: FutureBuilder<DocumentSnapshot>(
-        future: FirebaseFirestore.instance.collection('Medicamentos').doc(medicineName).get(),
-        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (!snapshot.hasData) return const Text('Cargando...');
-          final pharmacies = (snapshot.data!.get('Farmacias') as List).cast<String>();
-          return ListView.builder(
-            itemCount: pharmacies.length,
-            itemBuilder: (context, index) {
-              return FutureBuilder<DocumentSnapshot>(
-                future: getPharmacyData(pharmacies[index]),
-                builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-                  if (!snapshot.hasData) return const Text('Cargando...');
-                  final pharmacyData = snapshot.data!;
-                  if (pharmacyData.get('Departamento') == department && pharmacyData.get('Municipio') == municipality) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => MapPage(pharmacyName: pharmacies[index])),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  hexStringToColor("CB2B93"),
+                  hexStringToColor("9546C4"),
+                  hexStringToColor("5E61F4")
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter
+            )
+        ),
+        child: SafeArea(  // Agrega SafeArea aquí
+          child: FutureBuilder<DocumentSnapshot>(
+            future: FirebaseFirestore.instance.collection('Medicamentos').doc(medicineName).get(),
+            builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (!snapshot.hasData) return const Text('Cargando...');
+              final pharmacies = (snapshot.data!.get('Farmacias') as List).cast<String>();
+              final medicinePrice = snapshot.data!.get('PrecioEstandar');  // Obtener el precio estándar del medicamento
+              return Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      'Precio del medicamento: \$ $medicinePrice',  // Mostrar el precio estándar del medicamento
+                      style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white),  // Cambiado el color del texto a blanco
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      'NOTA: El precio mostrado solo es una referencia proporcionada por la Direccion Nacional de Medicamentos, este puede variar dependiendo del establecimiento que seleccione.',
+                      style: TextStyle(fontSize: 16.0, color: Colors.white),  // Cambiado el color del texto a blanco
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: pharmacies.length,
+                      itemBuilder: (context, index) {
+                        return FutureBuilder<DocumentSnapshot>(
+                          future: getPharmacyData(pharmacies[index]),
+                          builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                            if (!snapshot.hasData) return const Text('Cargando...');
+                            final pharmacyData = snapshot.data!;
+                            if (pharmacyData.get('Departamento') == department && pharmacyData.get('Municipio') == municipality) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => MapPage(pharmacyName: pharmacies[index])),
+                                  );
+                                },
+                                child: Card(
+                                  child: ListTile(
+                                    leading: Icon(Icons.local_pharmacy, color: Theme.of(context).primaryColor),
+                                    title: Text(pharmacies[index], style: Theme.of(context).textTheme.headline6),
+                                    trailing: Icon(Icons.arrow_forward_ios, color: Theme.of(context).primaryColor),
+                                  ),
+                                ),
+                              );
+                            } else {
+                              return Container();  // No mostrar nada si la farmacia
+                            }
+                          },
                         );
                       },
-                      child: ListTile(
-                        title: Text(pharmacies[index]),
-                      ),
-                    );
-                  } else {
-                    return Container();  // No mostrar nada si la farmacia no pertenece al departamento y municipio seleccionados
-                  }
-                },
+                    ),
+                  ),
+                ],
               );
             },
-          );
-        },
+          ),
+        ),
       ),
     );
   }
+
 }
